db3bcd1c4d16f8dea309978e589954b7ae75bd2e
Saidul Mondal <mondalsaidul3232@gmail.com>
2021-05-11 02:26:49 +0530

    commit-2

--- server/index.js
+++ server/index.js
@@ -1,11 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cookieParser = require("cookie-parser");
+
+dotenv.config()
+
+//set up server
 
 const app = express();
-
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT,() => console.log("App is running on port " + PORT))
 
-app.get("/test", (req, res) => {
-    res.send("All is well")
+app.use(express.json())
+app.use(cookieParser());
+
+//set up db
+
+const connectDB = process.env.DB
+
+mongoose.connect(connectDB, {
+    useUnifiedTopology:true,
+    useNewUrlParser:true
+}, (err) => {
+    if(err) return console.error(err)
+    console.log("Db connected successfully")
 })
+
+// set up router
+
+app.use("/auth", require("./routers/userRouter"))
+app.use("/customer", require("./routers/customerRouter"));
