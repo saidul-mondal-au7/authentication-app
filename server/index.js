59034ffceab26ca1d4f032863a2d5da127d4b57c
Saidul Mondal <mondalsaidul3232@gmail.com>
2021-05-10 22:58:48 +0530

    1st commit

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,() => console.log("App is running on port " + PORT))
+
+app.get("/test", (req, res) => {
+    res.send("All is well")
+})
